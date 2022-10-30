2260ce6fd3880949e23ae9f9855511f67f96528d
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-10-30 10:00:23 -0700

    Add files via upload

---
+++ Game984A.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rating_800;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class Game984A {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[(n-1)/2]);
+    }
+    
+}

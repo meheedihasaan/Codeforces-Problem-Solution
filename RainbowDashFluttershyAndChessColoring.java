bde31b4a106c152685a0e491f47cff54beff58cc
Muhammad Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2023-04-15 11:40:10 -0700

    Add files via upload

---
+++ RainbowDashFluttershyAndChessColoring.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rating_800;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class RainbowDashFluttershyAndChessColoring {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0){
+            int n = sc.nextInt();
+            System.out.println((n/2)+1);
+        }
+    }
+    
+}
